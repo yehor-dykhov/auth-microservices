7a3e309dc5d3f171aba44fb22948a4d6acd93985
Yehor Dykhov <hyoustone@yahoo.com>
2024-03-04 20:12:42 +0200

    Added CLI client

--- src/health-check-service/main.rs
+++ src/health-check-service/main.rs
@@ -3,7 +3,7 @@ use std::env;
 use authentication::auth_client::AuthClient;
 use authentication::{SignInRequest, SignOutRequest, SignUpRequest};
 use tokio::time::{sleep, Duration};
-use tonic::{IntoRequest, Request, Response};
+use tonic::{Request, Response};
 use uuid::Uuid;
 
 use crate::authentication::{SignInResponse, SignOutResponse, SignUpResponse, StatusCode};
@@ -18,7 +18,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // ::0 is required for Docker to work: https://stackoverflow.com/questions/59179831/docker-app-server-ip-address-127-0-0-1-difference-of-0-0-0-0-ip
     let auth_hostname = env::var("AUTH_SERVICE_HOST_NAME").unwrap_or("[::0]".to_owned());
 
-    // Establish connection when auth service
     let mut client = AuthClient::connect(format!("http://{}:50051", auth_hostname)).await?;
 
     loop {
@@ -32,7 +31,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         let response: Response<SignUpResponse> = client.sign_up(request).await?;
 
-        // Log the response
         println!(
             "SIGN UP RESPONSE STATUS: {:?}",
             StatusCode::from_i32(response.into_inner().status_code)
@@ -56,7 +54,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         let request: Request<SignOutRequest> = Request::new(SignOutRequest {
             session_token: response.session_token,
-        }); // Create a new `SignOutRequest`.
+        });
 
         let response: Response<SignOutResponse> = client.sign_out(request).await?;
 
@@ -65,7 +63,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             StatusCode::from_i32(response.into_inner().status_code)
         );
 
-        println!("--------------------------------------", );
+        println!("--------------------------------------",);
 
         sleep(Duration::from_secs(3)).await;
     }
